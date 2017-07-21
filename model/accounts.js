a151bd15565c3f1d56e3d8fe5a5bfd309fb67c7c
charlenebadi <charlene.badibanga@sky.uk>
2017-07-21 12:59:22 +0100

    added skeleton

---
+++ model/accounts.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var accountSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  emailConfirmation: String,
+  address: String,
+  postcode: String, //mixed doesn't seem to exist but 'string' might not let me input numbers
+  phoneNumber: Number,
+  age: { type: Number, min: 18, max: 65 },
+  dob: { type: Date, default: Date.now },
+  isloved: Boolean
+});
+mongoose.model('Account', accountSchema);
