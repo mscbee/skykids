d89708ab9b01a7fb88feed5c8b2ddde249b2b8f2
“Conor” <conor.okus@hotmail.co.uk>
2017-07-24 16:07:23 +0100

    Setup and run EJS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', message: 'Welcome to express',
+names: ['Conor', 'Luke', 'Charlene', 'Sandi', 'Lisa', 'Alex', 'Jas'] });
 });
 
 module.exports = router;
