e757f0373ee28dd2dd057c75487994b8e64d48bc
sandiya-baranidaran <sandiya.baranidaran@sky.uk>
2017-07-26 17:02:01 +0100

    Merge issues, account route now added to app.js

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-
-var routes = require('./routes/index');
-var blobs = require('./routes/accounts');
-
-app.use('/', routes);
-app.use('/accounts', accounts);
