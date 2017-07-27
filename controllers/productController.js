3c945b178160a0732641dbadc472ad58627ed450
lukeworgerSky <luke.worger@sky.uk>
2017-07-27 16:48:29 +0100

    added some styling, and used actual relevant text

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,8 +2,8 @@ var Product = require('../model/product');
 
 // Route to homepage
 exports.index = function(req, res){
-    res.render('../views/catalog.ejs', { title: 'Express', message: 'Welcome to express',
-    names: ['Conor', 'Luke', 'Charlene', 'Sandi', 'Lisa', 'Alex', 'Jas'] });
+    res.render('../views/catalog.ejs', { title: 'Catalog', message: 'Welcome to the SkyKids store',
+    names: ['products', 'basket', 'account'] });
 }
 
 // Display list of all products
