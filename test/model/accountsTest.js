a03eb170ebb421c675b4a54d6a5a0d236c00e123
lukeworgerSky <luke.worger@sky.uk>
2017-08-01 13:47:13 +0100

    testing jenkins with failing test

--- test/model/accountsTest.js
+++ test/model/accountsTest.js
@@ -1,15 +1,17 @@
 var should = require('chai').should();
 var assert = require('chai').assert;
+var expect = require('chai').expect;
 
 describe('tests for account model', function() {
 
   var account;
 
   beforeEach(function(done) {
-        done() 
+        done()
       });
 
   it('account exists', function(done) {
+    expect(2).to.equal(5);
     done();
   });
 
