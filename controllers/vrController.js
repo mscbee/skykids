fa1ead59015fd0b2edd8f950887869eff465dbb2
lukeworgerSky <luke.worger@sky.uk>
2017-08-01 11:39:21 +0100

    changed route to get data in from vr

--- controllers/vrController.js
+++ controllers/vrController.js
@@ -1,9 +1,8 @@
 var vrController = {};
 
 vrController.sendOrder = function(req, res){
-  console.log(req.data);
+  console.log(req.body);
   // res.render('register');
 }
 
 module.exports = vrController;
-
