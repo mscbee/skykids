530f660ff149c1448c31e65225b3cfecf0d7c831
sandiya-baranidaran <sandiya.baranidaran@sky.uk>
2017-08-01 15:27:57 +0100

    updated payment

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,7 +1,12 @@
-//card validation using luhn's algorithm
+var e = document.getElementById('parent');
 
-var cardvalidate = require('card'),
-	card = cardvalidate('xxxxxxxxxxxxxxxx'); //hard code numbers to test if it works
+e.onmouseover = function () {
+  document.getElementById('popup').style.display = 'block';
+}
+
+e.onmouseout = function () {
+  document.getElementById('popup').style.display = 'none';
+}
 
 // function is linked to onclick event in views/payments.ejs
 //and redirects user to views/paymentsuccess.ejs
@@ -11,7 +16,7 @@ function makePayment() {
   window.location="../../views/paymentSuccess"; //takes us to views/paymentSuccess page
 
 }
-
+ var telephone = document.getElementById('phoneNumber').value;
 //this only works with node index.js command
 function sendText() {
   var accountSid = 'AC2fea5b00e75f029c0bb8657e9a5e0c32'; // Your Account SID from www.twilio.com/console
@@ -22,7 +27,7 @@ function sendText() {
 
   client.messages.create({
       body: 'Thanks for your order!',
-      to: '+447944899080',  // Text this number - hard-coded CB's number
+      to: telephone,  // Text to the number from form
       from: '+441158246021' // From a valid Twilio number
     })
 }
