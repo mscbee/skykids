87cdf592ee38a07665553ccf4a9442508a96a63a
lukeworgerSky <luke.worger@sky.uk>
2017-08-03 13:41:16 +0100

    products now shown in warehouse grid

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -6,7 +6,6 @@ warehouseController.index = function(req, res){
 
   Order.find({},function(err,orders){
     if (!err){
-      console.log(orders);
       res.render('warehouse',{orders: orders});
     }
     else {
