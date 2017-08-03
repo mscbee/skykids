65195e1fe743fd1c41fe74abd7a71d94c7657875
lukeworgerSky <luke.worger@sky.uk>
2017-08-03 12:05:00 +0100

    jenkins test

--- routes/cart.js
+++ routes/cart.js
@@ -10,7 +10,7 @@ var cart_controller = require('../controllers/cartController');
 //Router to render cart view
 router.get('/cart', cart_controller.index);
 
-router.get('/vrCart', cart_controller.vrIndex);
+router.get('/vrCart', cart_controller.vrIndex); 
 
 //Router to perform cart operation
 router.get('/add-to-cart/:id', cart_controller.addToCart);
